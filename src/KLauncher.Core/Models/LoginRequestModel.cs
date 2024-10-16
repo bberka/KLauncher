45c4f9b1233cfdd3680f2880802f2a15808fb1db
Berkay <77205615+bberka@users.noreply.github.com>
2024-10-16 21:46:12 +0300

    refactor: rename LoginRequest.cs

---
+++ src/KLauncher.Core/Models/LoginRequestModel.cs
@@ -0,0 +1,5 @@
+﻿using KLauncher.Core.Manager;
+
+namespace KLauncher.Core.Models;
+
+public sealed record class LoginRequestModel(string Username, string Password, string PasswordHash, bool RememberMe);
