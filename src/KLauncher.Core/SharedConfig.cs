49bc5070691240a7ca5948cd672d622f9de31527
Berkay <77205615+bberka@users.noreply.github.com>
2024-10-16 21:46:28 +0300

    feat: SharedConfig.cs

--- src/KLauncher.Core/SharedConfig.cs
+++ src/KLauncher.Core/SharedConfig.cs
@@ -4,5 +4,13 @@ namespace KLauncher.Core;
 
 public static class SharedConfig
 {
-    public static readonly HashType HashType = HashType.XxHash;
+  public static readonly HashType HashType = HashType.XxHash;
+
+  public static readonly string[] IgnoreGameFileExtensions = {
+    ".log"
+  };
+
+  public const long MaxGameFileSize = 1024 * 1024 * 1024; //1GB
+
+  public const bool CheckZeroByteGameFiles = true;
 }
